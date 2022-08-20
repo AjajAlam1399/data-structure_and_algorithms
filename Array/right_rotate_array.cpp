b4e8f8c7288a338533e77fd35648c58e694cb009
Ajaj Alam <2020umt1399@mnit.ac.in>
2022-08-20 16:06:55 +0530

    right rotate array

---
+++ Array/right_rotate_array.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(int a){
+    cout<<a<<" ";
+}
+
+int main(){
+
+    vector<int>v={1,2,3,4,5,6,7,8};
+
+    rotate(v.begin(),v.end()-2,v.end());
+
+    for_each(v.begin(),v.end(),print);
+}
