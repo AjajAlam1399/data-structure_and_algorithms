1dcccc15e305f813326d24847611749bc7a1d5f6
Ajaj Alam <2020umt1399@mnit.ac.in>
2022-08-31 15:32:45 +0530

    inorder traversal

---
+++ Tree/inorder-traversal.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+using namespace std;
+
+class TreeNode
+{
+
+public:
+    int data;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode()
+    {
+        data = 0;
+        left = NULL;
+        right = NULL;
+    }
+    TreeNode(int data)
+    {
+        this->data = data;
+        left = right = NULL;
+    }
+};
+
+class BST
+{
+public:
+    TreeNode *root;
+    BST()
+    {
+        root = NULL;
+    }
+
+    bool isEmpty()
+    {
+        if (root == NULL)
+        {
+            return true;
+        }
+        else
+            return false;
+    }
+
+    void insertNode(TreeNode *newnode)
+    {
+
+        if (isEmpty())
+        {
+            root = newnode;
+            cout << "node inseted at the root !" << endl;
+            return;
+        }
+        else
+        {
+            TreeNode *temp = root;
+            while (temp != NULL)
+            {
+                if (temp->data == newnode->data)
+                {
+                    cout << "duplicate is not allowed please try with the diffrent node" << endl;
+                    break;
+                }
+                else if (temp->data > newnode->data && temp->left == NULL)
+                {
+                    temp->left = newnode;
+                    cout << "node inserted as left child" << endl;
+                    break;
+                }
+                else if (temp->data > newnode->data)
+                {
+                    temp = temp->left;
+                }
+                else if (temp->data < newnode->data && temp->right == NULL)
+                {
+                    temp->right = newnode;
+                    cout << "node inserted as right child !" << endl;
+                    break;
+                }
+                else
+                {
+                    temp = temp->right;
+                }
+            }
+        }
+    }
+    void inorderTraversal(TreeNode *r)
+    {
+        if (r = NULL)
+        {
+            return;
+        }
+        inorderTraversal(r->left);
+        cout << r->data << " ";
+        inorderTraversal(r->right);
+    }
+};
