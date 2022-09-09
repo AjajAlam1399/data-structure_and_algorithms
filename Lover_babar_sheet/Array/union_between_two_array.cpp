3c4eb8a66c134b2d6b8751c538500afa33d34af0
Ajaj Alam <2020umt1399@mnit.ac.in>
2022-09-09 16:45:42 +0530

    array

---
+++ Lover_babar_sheet/Array/union_between_two_array.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int arr1[] = {5, 3, 9, 1, 2, 8};
+    int size1 = sizeof(arr1) / sizeof(arr1[0]);
+    int arr2[] = {7, 4, 1, 9, 0, 4, 3, 1};
+    int size2 = sizeof(arr2) / sizeof(arr2[0]);
+
+    vector<int> v;
+
+    copy(arr1, arr1 + size1, back_inserter(v));
+    copy(arr2, arr2 + size2, back_inserter(v));
+
+    sort(v.begin(), v.end());
+
+    for (auto it : v)
+        cout << it << " ";
+    cout << endl;
+
+    for (int i = 0; i < v.size(); i++)
+    {
+        if (v[i]!=v[i+1])
+        {
+            cout << v[i] << " ";
+        }
+    }
+
+}
