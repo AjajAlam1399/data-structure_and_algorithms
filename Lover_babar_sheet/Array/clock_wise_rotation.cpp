3c9429b0b3b93fa6c5645b30918204706d30a874
Ajaj Alam <2020umt1399@mnit.ac.in>
2022-09-09 16:50:26 +0530

    array

---
+++ Lover_babar_sheet/Array/clock_wise_rotation.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int arr1[] = {5, 3, 9, 1, 2, 8};
+    int size1 = sizeof(arr1) / sizeof(arr1[0]);
+
+    rotate(arr1, arr1 +1, arr1 + size1);
+
+    for (auto it : arr1)
+        cout << it << " ";
+}
