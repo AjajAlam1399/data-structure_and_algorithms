6f76a38a10f088a7e5646829050bbc488092b820
Ajaj Alam <2020umt1399@mnit.ac.in>
2022-09-24 17:25:04 +0530

    array

---
+++ STRIVER_SHEET/ARRAY/merg_sorted_array.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
+    {
+        int count = 0;
+        for (int i = m; i < m + n; i++)
+        {
+            nums1[i] = nums2[count];
+            count++;
+        }
+
+        sort(nums1.begin(), nums1.end());
+    }
+};
