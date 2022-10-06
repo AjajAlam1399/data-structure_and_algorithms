2ffc132d5d48528aeda784e2573d0ab204fe8b0d
Ajaj Alam <2020umt1399@mnit.ac.in>
2022-10-07 00:54:06 +0530

    array

---
+++ pr.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int findPairs(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        
+        unordered_map<int,int>m;
+        
+        
+        
+        map<pair<int,int>,int>result;
+        
+        for(int i=0;i<nums.size();i++){
+            if(m.find(nums[i]-k)!=m.end()){
+                result[make_pair(nums[i]-k,nums[i])]++;
+            };
+            if(m.find(nums[i])==m.end()){
+                m[nums[i]]++;
+            }
+        }
+        return result.size();
+    }
