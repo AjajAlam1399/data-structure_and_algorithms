601d4fe8f59a6f6a025a8e20ffc413fc0a343c89
Ajaj Alam <2020umt1399@mnit.ac.in>
2022-10-12 22:32:15 +0530

    array

--- Lover_babar_sheet/Array/duplicate_in_array_leet_code.cpp
+++ Lover_babar_sheet/Array/duplicate_in_array_leet_code.cpp
@@ -1,20 +1,19 @@
 #include <bits/stdc++.h>
+using namespace std;
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        unordered_map<int, int> m;
+         int n=nums.size();
         
-        for(int i=0;i<nums.size();i++){
-            m[nums[i]]++;
+        for(int i=0;i<n;i++){
+            nums[nums[i]%n]+=n;
         }
-        int result=0;
         
-        for(auto it : m){
-            if(it.second>1){
-                result=it.first;
-                break;
-            }
+        for(int i=0;i<n;i++){
+           if( nums[i]/n>1){
+               return i;
+           }
         }
-        return result;
+        return -1;
     }
 };
