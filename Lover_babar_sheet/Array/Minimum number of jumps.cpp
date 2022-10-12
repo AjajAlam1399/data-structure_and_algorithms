ad23c2a51cdfa86b7a09daaf3d046b981fe09f64
Ajaj Alam <2020umt1399@mnit.ac.in>
2022-10-12 20:41:46 +0530

    array

---
+++ Lover_babar_sheet/Array/Minimum number of jumps.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// Function to return minimum number of jumps to end of array
+
+class Solution{
+  public:
+    int minJumps(int arr[], int n){
+        // Your code here
+        if(n<=1){
+            return 0;
+        }
+        if(arr[0]==0){
+            return -1;
+        }
+        int step=arr[0];
+        int maxR=arr[0];
+        int jump=1;
+        
+        for(int i=1;i<n;i++){
+            if(i==n-1){
+                return jump;
+            }
+            maxR=max(maxR,i+arr[i]);
+            step--;
+            if(step==0){
+                jump++;
+                if(i>=maxR){
+                    return -1;
+                }
+                step=maxR-i;
+            }
+        }
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,i,j;
+        cin>>n;
+        int arr[n];
+        for(int i=0; i<n; i++)
+            cin>>arr[i];
+        Solution obj;
+        cout<<obj.minJumps(arr, n)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
