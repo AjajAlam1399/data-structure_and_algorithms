3068a045181e1c7c7e5ae9982adb8869cb6124c1
Ajaj Alam <2020umt1399@mnit.ac.in>
2022-10-17 22:59:21 +0530

    DP

---
+++ Dynamic-programming/3_coding_ninja_Frog Jump.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#include <bits/stdc++.h> 
+int total_energy(int n,vector<int>heights,vector<int>&dp){
+    if(n==0){
+        return 0;
+    }
+    if(dp[n]!=-1){
+        return dp[n];
+    }
+    int left=total_energy(n-1,heights,dp)+abs(heights[n]-heights[n-1]);
+    int right=INT_MAX;
+    if(n>1){
+        right=total_energy(n-2,heights,dp)+abs(heights[n]-heights[n-2]);
+    }
+    return dp[n]=min(left,right);
+}
+int frogJump(int n, vector<int> &heights)
+{
+    // Write your code here.
+    vector<int>dp(n+1,-1);
+    return total_energy(n-1,heights,dp);
+}
