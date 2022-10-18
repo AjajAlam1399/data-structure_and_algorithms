fe246f5f150ddef55a01e84c84b94e6dbafcf3fc
Ajaj Alam <2020umt1399@mnit.ac.in>
2022-10-18 17:22:51 +0530

    DP

--- Dynamic-programming/5_Max Sum without Adjacents.cpp
+++ Dynamic-programming/5_Max Sum without Adjacents.cpp
@@ -69,3 +69,12 @@ int findMaxSum(int *arr, int n) {
 	    }
 	    return current;
 	}
+
+    int main(){
+        #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+        #endif
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+    }
