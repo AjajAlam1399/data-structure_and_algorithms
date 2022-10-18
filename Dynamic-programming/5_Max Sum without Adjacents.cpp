9b14a0a37f094052eeff519ceba0daa918700b13
Ajaj Alam <2020umt1399@mnit.ac.in>
2022-10-18 17:20:57 +0530

    DP

--- Dynamic-programming/5_Max Sum without Adjacents.cpp
+++ Dynamic-programming/5_Max Sum without Adjacents.cpp
@@ -29,3 +29,43 @@ public:
 
 // tabulation method 
 
+int findMaxSum(int *arr, int n) {
+	    // code here
+	    vector<int>dp(n+1,-1);
+	   // return max_sum(arr,n-1,dp);
+	    dp[0]=arr[0];
+	    dp[1]=max(arr[0],arr[1]);
+	    for(int i=2;i<n;i++){
+	        int pick=arr[i]+dp[i-2];
+	        int not_pick=dp[i-1];
+	        dp[i]=max(pick,not_pick);
+	    }
+	    return dp[n-1];
+	}
+
+
+// space optimization 
+
+
+	int findMaxSum(int *arr, int n) {
+	    // code here
+	    vector<int>dp(n+1,-1);
+	   // return max_sum(arr,n-1,dp);
+	   if(n==1){
+	       return arr[0];
+	   }
+	   else if(n==2){
+	       return max(arr[0],arr[1]);
+	   }
+	    int prev2=arr[0];
+	    int prev1=max(arr[0],arr[1]);
+	    int current;
+	    for(int i=2;i<n;i++){
+	        int pick=arr[i]+prev2;
+	        int not_pick=prev1;
+	        current=max(pick,not_pick);
+	        prev2=prev1;
+	        prev1=current;
+	    }
+	    return current;
+	}
