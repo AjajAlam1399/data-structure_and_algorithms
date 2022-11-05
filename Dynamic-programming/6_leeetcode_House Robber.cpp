19ae8662a6c761b91da45644e7f00d93268268db
Ajaj Alam <2020umt1399@mnit.ac.in>
2022-11-05 23:08:47 +0530

    dp

--- Dynamic-programming/6_leeetcode_House Robber.cpp
+++ Dynamic-programming/6_leeetcode_House Robber.cpp
@@ -26,12 +26,6 @@ public:
 };
 
 int main(){
-
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt","r",stdin);
-    freopen("output.txt","w",stdout);
-    #endif
-
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
