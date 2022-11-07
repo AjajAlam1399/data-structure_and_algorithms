09dd882a4cb3f8a5ff8b92a1d0d13c19f5595cae
Ajaj Alam <2020umt1399@mnit.ac.in>
2022-11-07 23:18:57 +0530

    tree

---
+++ Tree/Tree-traversal/12_Boundary Traversal of binary tree.cpp
@@ -0,0 +1,179 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if(currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if(currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+
+
+
+
+
+
+
+// } Driver Code Ends
+/* A binary tree Node
+struct Node
+{
+    int data;
+    Node* left, * right;
+}; */
+
+class Solution {
+    bool isleaf(Node* root){
+        if(root->left==NULL && root->right==NULL){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    void left_traversal(Node* root,vector<int>&ans){
+        Node* curr=root->left;
+        while(curr){
+            if(!isleaf(curr)) ans.push_back(curr->data);
+            if(curr->left) curr=curr->left;
+            else curr=curr->right;
+        }
+    }
+    void right_traversal(Node* root,vector<int>&ans){
+        vector<int>temp;
+        Node* curr=root->right;
+        while(curr){
+            if(!isleaf(curr)) temp.push_back(curr->data);
+            if(curr->right) curr=curr->right;
+            else curr=curr->left;
+        }
+        for(auto it=temp.rbegin();it!=temp.rend();it++){
+            ans.push_back(*it);
+        }
+    }
+    void inorder(Node* root,vector<int>&ans){
+        if(isleaf(root)) {ans.push_back(root->data);
+            return ;
+        }
+        if(root->left) inorder(root->left,ans);
+        if(root->right) inorder(root->right,ans);
+    }
+public:
+    vector <int> boundary(Node *root)
+    {
+        //Your code here
+        vector<int>ans;
+        if(root==NULL){
+            return ans;
+        }
+        if(!isleaf(root)) ans.push_back(root->data);
+        left_traversal(root,ans);
+        inorder(root,ans);
+        right_traversal(root,ans);
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+/* Driver program to test size function*/
+
+int main() {
+    int t;
+    string tc;
+    getline(cin, tc);
+    t=stoi(tc);
+    while(t--)
+    {
+        string s ,ch;
+        getline(cin, s);
+        Node* root = buildTree(s);
+        Solution ob;
+        vector <int> res = ob.boundary(root);
+        for (int i : res) cout << i << " ";
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
