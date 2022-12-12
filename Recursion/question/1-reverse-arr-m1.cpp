bc069778f066b4111dbb8d30095cebefca790d7e
Ajaj Alam <2020umt1399@mnit.ac.in>
2022-12-12 10:19:11 +0530

    recursion

---
+++ Recursion/question/1-reverse-arr-m1.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void fun(int arr[],int l,int r){
+
+    if(l>=r)return;
+
+    swap(arr[l],arr[r]);
+    fun(arr,l+1,r-1);
+}
+int main(){
+    int arr[]={1,2,3,4,5};
+    fun(arr,0,sizeof(arr)/sizeof(arr[0])-1);
+
+    for(auto i : arr) cout<<i<<" ";
+}
