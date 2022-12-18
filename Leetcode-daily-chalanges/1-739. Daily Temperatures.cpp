a833d6ea3fe2f3584918f3c1e8d70c7664af13a8
Ajaj Alam <2020umt1399@mnit.ac.in>
2022-12-18 16:16:47 +0530

    leetcode

---
+++ Leetcode-daily-chalanges/1-739. Daily Temperatures.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> dailyTemperatures(vector<int> &temperatures)
+    {
+        vector<int> ans(temperatures.size());
+
+        stack<pair<int, int>> st;
+
+        for (int i = temperatures.size() - 1; i >= 0; i--)
+        {
+
+            while (!st.empty() && st.top().first <= temperatures[i])
+            {
+                st.pop();
+            }
+
+            if (!st.empty())
+            {
+                int ind = st.top().second;
+                ans[i] = ind - i;
+            }
+            else
+            {
+                ans[i] = 0;
+            }
+            st.push({temperatures[i], i});
+        }
+
+        return ans;
+    }
+};
