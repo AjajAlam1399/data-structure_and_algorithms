d8dc32bdef5f0ec267e470ae30b4dd6cd3ce3bfd
Ajaj Alam <2020umt1399@mnit.ac.in>
2022-12-20 21:18:10 +0530

    stack

---
+++ Stack/Question/5-Smallest number on left.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    vector<int> leftSmaller(int n, int a[]){
+        // code here
+        vector<int>ans(n,-1);
+        
+        stack<int>st;
+        
+        for(int i=0;i<n;i++){
+            
+            while(!st.empty() && st.top()>=a[i]){
+                st.pop();
+            }
+            
+            if(!st.empty()){
+                ans[i]=st.top();
+            }
+            
+            st.push(a[i]);
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i = 0;i < n;i++)
+            cin>>a[i];
+        
+        Solution ob;
+        vector<int> ans = ob.leftSmaller(n, a);
+        for(int i = 0;i < n;i++)
+            cout<<ans[i]<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
