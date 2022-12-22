7365fb00da7151a4a9a37586b2a6d0aec253bbd7
Ajaj Alam <2020umt1399@mnit.ac.in>
2022-12-22 10:45:54 +0530

    heap

---
+++ Heap/3-Kth Largest Element in an Array.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int findKthLargest(vector<int> &nums, int k)
+    {
+
+        priority_queue<int, vector<int>, greater<int>> pq;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (pq.size() < k)
+            {
+                pq.push(nums[i]);
+            }
+            else
+            {
+                if (nums[i] > pq.top())
+                {
+                    pq.pop();
+                    pq.push(nums[i]);
+                }
+            }
+        }
+        return pq.top();
+    }
+};
