a84b66ae125e8c43e6f5c0dcd48ab5ad73e56ada
Ajaj Alam <2020umt1399@mnit.ac.in>
2022-12-24 10:46:43 +0530

    matrix

---
+++ Matrix/2-leetcode-74. Search a 2D Matrix.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    bool searchMatrix(vector<vector<int>> &matrix, int target)
+    {
+
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int l = 0;
+        int r = m * n - 1;
+
+        while (l <= r)
+        {
+            int mid = l + (r - l) / 2;
+            if (matrix[mid / n][mid % n] == target)
+            {
+                return true;
+            }
+            else if (matrix[mid / n][mid % n] > target)
+            {
+                r = mid - 1;
+            }
+            else
+            {
+                l = mid + 1;
+            }
+        }
+
+        return false;
+    }
+};
