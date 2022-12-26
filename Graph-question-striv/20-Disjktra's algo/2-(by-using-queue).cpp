28b7d12cdeb47ea58c1cefbbc76d0c2a5bd1e949
Ajaj Alam <2020umt1399@mnit.ac.in>
2022-12-26 09:10:22 +0530

    graph

---
+++ Graph-question-striv/20-Disjktra's algo/2-(by-using-queue).cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+	public:
+	//Function to find the shortest distance of all the vertices
+    //from the source vertex S.
+    vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    {
+        // Code here
+        
+        queue<pair<int,int>>q;
+        
+        vector<int>dist(V,1e7);
+        
+        dist[S]=0;
+        
+        q.push({0,S});
+        
+        while(!q.empty()){
+            int node=q.front().second;
+            int srcDist=q.front().first;
+            q.pop();
+            
+            for(auto it : adj[node]){
+                int adjNode=it[0];
+                int W=it[1];
+                
+                if(dist[adjNode]>srcDist+W){
+                    dist[adjNode]=srcDist+W;
+                    q.push({srcDist+W,adjNode});
+                }
+            }
+        }
+        
+        return dist;
+    }
+};
+
