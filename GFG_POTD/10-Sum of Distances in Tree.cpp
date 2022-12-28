fd53976983c4514c3ff398e7c3c8b25fbad9bbb8
Ajaj Alam <2020umt1399@mnit.ac.in>
2022-12-28 21:01:47 +0530

    tree

---
+++ GFG_POTD/10-Sum of Distances in Tree.cpp
@@ -0,0 +1,41 @@
+class Solution
+{
+public:
+    // TIME COMPLEXITY:- O(N)
+    // SPACE COMPLEXITY:- O(N)
+    int dfs(int v, int par, vector<int> adj[], vector<int> &subtree)
+    {
+        int curr = 0;
+        for (auto u : adj[v])
+        {
+            if (u != par)
+            {
+                curr += dfs(u, v, adj, subtree);
+                curr += subtree[u];
+                subtree[v] += subtree[u];
+            }
+        }
+        return curr;
+    }
+    void dfs(int v, int par, vector<int> adj[], vector<int> &ans, vector<int> &subtree, int now)
+    {
+        ans[v] = now;
+        for (auto u : adj[v])
+        {
+            if (u != par)
+                dfs(u, v, adj, ans, subtree, now - subtree[u] + subtree[0] - subtree[u]);
+        }
+    }
+    vector<int> sumOfDistancesInTree(int n, vector<vector<int>> &edges)
+    {
+        vector<int> ans(n), subtree(n, 1), adj[n];
+        for (auto v : edges)
+        {
+            adj[v[0]].push_back(v[1]);
+            adj[v[1]].push_back(v[0]);
+        }
+        int root_ans = dfs(0, -1, adj, subtree);
+        dfs(0, -1, adj, ans, subtree, root_ans);
+        return ans;
+    }
+};
