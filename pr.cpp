fe54ecb09e2ac574cd65848c9447c8b96ab1e5d0
Ajaj Alam <2020umt1399@mnit.ac.in>
2023-01-02 15:57:33 +0530

    leetcode

--- pr.cpp
+++ pr.cpp
@@ -1,121 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int globalspace = 10;
-
-class node
+int main()
 {
-public:
-    node *left;
-    node *right;
-    int data;
-    node(int data)
+    int num=27;
+    vector<int> search;
+    unordered_map<int, string> no;
+    no[1] = "I";
+    search.push_back(1);
+    no[4] = "IV";
+    search.push_back(4);
+    no[5] = "V";
+    search.push_back(5);
+    no[9] = "IX";
+    search.push_back(9);
+    no[10] = "X";
+    search.push_back(10);
+    search.push_back(27);
+    no[40] = "XL";
+    search.push_back(40);
+    no[50] = "L";
+    search.push_back(50);
+    no[90] = "XC";
+    search.push_back(90);
+    no[100] = "C";
+    search.push_back(100);
+    no[400] = "CD";
+    search.push_back(400);
+    no[500] = "D";
+    search.push_back(500);
+    no[900] = "CM";
+    search.push_back(900);
+    no[1000] = "M";
+    search.push_back(1000);
+
+    string ans = " ";
+    while (num>=0)
     {
-        this->data = data;
-        left = right = NULL;
-    }
-};
-
-class BST
-{
-public:
-    node *root;
-    BST()
-    {
-        root = NULL;
-    }
-    node *insert_recursively(node *root, node *newnode)
-    {
-        if (root == NULL)
-        {
-            root = newnode;
-            return root;
-        }
-        else if (root->data > newnode->data)
-        {
-            root->left = insert_recursively(root->left, newnode);
-        }
-        else if (root->data < newnode->data)
-        {
-            root->right = insert_recursively(root->right, newnode);
-        }
-        else
-        {
-            return root;
-        }
-        return root;
-    }
-    void display(node *root, int space)
-    {
-        if (root == NULL)
-        {
-            return;
-        }
-        space += globalspace;
-        display(root->right, space);
-        for (int i = globalspace; i < space; i++)
-        {
-            cout << " ";
-        }
-        cout << root->data << endl;
-        display(root->left, space);
-    }
-
-    vector<int> preorder(node *root)
-    {
-        vector<int> result;
-        if (root == NULL)
-        {
-            return result;
-        }
-        stack<node *> s;
-        s.push(root);
-        while (!s.empty())
-        {
-            root = s.top();
-            s.pop();
-            result.push_back(root->data);
-            if (root->right != NULL)
-            {
-                s.push(root->right);
-            }
-            if (root->left != NULL)
-            {
-                s.push(root->left);
-            }
-        }
-        return result;
-    }
-};
-
-void traversal(node* root,vector<int>preorder,vector<int>&inorder,vector<int>&postorder){
-    node* temp=root;
-
-    stack<pair<node*,int>>st;
-
-    st.push({temp,0});
-
-    while(!st.empty()){
-        temp=st.top().first;
-        int l=st.top().second;
-        st.pop();
         
-        if(l==0){
-            preorder.push_back(temp->data);
-            st.push({temp,l+1});
-            if(temp->left){
-                st.push({temp->left,0});
-            }
-        }
-        else if(l==1){
-            inorder.push_back(temp->data);
-            st.push({temp,l+1});
-            if(temp->right){
-                st.push({temp->right,0});
-            }
-        }
-        else{
-            postorder.push_back(temp->data);
-        }
+        cout<<num<<" ";
+        auto iter = lower_bound(search.begin(), search.end(), num);
+        cout<<*iter;
+        int ind = distance(search.begin(), iter);
+        // cout<<search[ind];
+        ans += no[search[ind]];
+        num -= search[ind];
+        cout<<endl;
     }
 }
