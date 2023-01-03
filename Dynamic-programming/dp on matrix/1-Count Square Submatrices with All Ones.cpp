4ddc0c3735e4aeddb5c8e46f092ca3c4295bcf58
Ajaj Alam <2020umt1399@mnit.ac.in>
2023-01-03 16:46:49 +0530

    dp on grid

---
+++ Dynamic-programming/dp on matrix/1-Count Square Submatrices with All Ones.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int countSquares(vector<vector<int>> &matrix)
+    {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+
+        int count = 0;
+
+        for (int i = 0; i < m; i++)
+        {
+            if (matrix[i][0] == 1)
+            {
+                dp[i][0] = 1;
+            }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            if (matrix[0][i] == 1)
+            {
+                dp[0][i] = 1;
+            }
+        }
+
+        for (int i = 1; i < m; i++)
+        {
+            for (int j = 1; j < n; j++)
+            {
+
+                if (matrix[i][j] == 1)
+                {
+                    dp[i][j] = 1 + min(dp[i][j - 1],
+                                       min(dp[i - 1][j], dp[i - 1][j - 1]));
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                count += dp[i][j];
+            }
+        }
+
+        return count;
+    }
+};
