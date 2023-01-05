0e8168c7742c7a9cdc3572ad653dc9e59c356db4
Ajaj Alam <2020umt1399@mnit.ac.in>
2023-01-05 11:34:07 +0530

    dp on string

---
+++ Dynamic-programming/15_DP-ON-STRING/5-Maximum Length of Repeated Subarray leetcode.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int findLength(vector<int>& nums1, vector<int>& nums2) {
+        int n=nums1.size();
+        int m=nums2.size();
+         vector<int>curr(m+1,0);
+        vector<int>prev(m+1,0);
+        
+        int maxl=0;
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(nums1[i-1]==nums2[j-1]){
+                    curr[j]=1+prev[j-1];
+                    maxl=max(maxl,curr[j]);
+                }
+                else{
+                    curr[j]=0;
+                }
+                
+            }
+            prev=curr;
+        }
+        
+        return maxl;
+    }
+};
