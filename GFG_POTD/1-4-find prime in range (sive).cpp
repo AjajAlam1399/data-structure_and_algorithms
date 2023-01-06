73d9020f4dd5162b4c5326ec7b8b7c15fd9d7375
Ajaj Alam <2020umt1399@mnit.ac.in>
2023-01-06 14:06:23 +0530

    .

---
+++ GFG_POTD/1-4-find prime in range (sive).cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> isprime(1e4, 1);
+void fun()
+{
+    for (int i = 2; i <= 9999; i++)
+    {
+        if (isprime[i] = 1)
+        {
+            for (int j = i * i; j <= 9999; j += i)
+            {
+                isprime[j] = 0;
+            }
+        }
+    }
