8c87fab8a4ad8a5dd6cba7b71a019dad21adf9bc
Ajaj Alam <2020umt1399@mnit.ac.in>
2023-01-12 20:08:49 +0530

    tree

---
+++ Tree/Tree-traversal/39-Kth largest element in BST.cpp
@@ -0,0 +1,172 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Function to Build Tree
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = new Node(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+
+// } Driver Code Ends
+/*The Node structure is defined as
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+
+// return the Kth largest element in the given BST rooted at 'root'
+class Solution
+{
+    public:
+    int kthLargest(Node *root, int K)
+    {
+        //Your code here
+        int siz=0;
+        
+        size(root,siz);
+        
+        int ans=-1;
+        
+        int count=0;
+        
+        K=siz-K+1;
+        
+        bool temp=fun(root,count,K,ans);
+        return ans;
+    }
+    
+    void size(Node* root,int &ans){
+        if(root==NULL){
+            return ;
+        }
+        ans++;
+        size(root->left,ans);
+        size(root->right,ans);
+    }
+    
+    bool fun(Node* root,int &count,int k,int  &ans ){
+        
+        if(root==NULL){
+            return false;
+        }
+
+        if(fun(root->left,count,k,ans)){
+            return true;
+        }
+        count++;
+        
+        if(k==count){
+            ans=root->data;
+            return true;
+        }
+        
+        if(fun(root->right,count,k,ans)){
+            return true;
+        }
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    getchar();
+    
+    while(t--)
+    {
+        string s;
+        getline(cin,s);
+        Node* head = buildTree(s);
+        
+        int k;
+        cin>>k;
+        getchar();
+        
+        Solution ob;
+        cout << ob.kthLargest( head, k ) << endl;
+    }
+    return 1;
+}
+// } Driver Code Ends
