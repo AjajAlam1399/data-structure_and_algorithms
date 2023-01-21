5e639957e1466fe5b0a6e6140f081964b9efc0a5
Ajaj Alam <2020umt1399@mnit.ac.in>
2023-01-21 20:13:22 +0530

    smallest window

--- Subarray/smallest window/3-Smallest window in a string containing all the characters of another string.cpp
+++ Subarray/smallest window/3-Smallest window in a string containing all the characters of another string.cpp
@@ -1,81 +1,75 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string smallestWindow(string s, string t)
-{
-    // Your code here
-    unordered_map<char, int> map1;
+class Solution {
+public:
+    string minWindow(string s, string t) {
 
-    for (int i = 0; i < t.size(); i++)
-    {
-        char ch = t[i];
-        map1[ch]++;
-    }
-
-    int totalcnt = t.size();
-    int currcnt = 0;
-
-    unordered_map<char, int> map2;
-
-    int i = -1;
-    int j = -1;
-
-    string ans = "";
-
-    while (true)
-    {
-        bool flag1 = false;
-        bool flag2 = false;
-
-        int size = s.size() - 1;
-
-        while (i < size && currcnt < totalcnt)
-        {
-            i++;
-            char ch = s[i];
-            map2[ch]++;
-
-            if (map2[ch] <= map1[ch])
-            {
-                currcnt++;
-            }
-            flag1 = true;
+        unordered_map<char,int>map1;
+        
+        for(int i=0;i<t.size();i++){
+            char ch=t[i];
+            map1[ch]++;
         }
+        
+        int totalcnt=t.size();
+        int currcnt=0;
+        
+        unordered_map<char,int>map2;
+        
+        int i=-1;
+        int j=-1;
 
-        while (j < i && currcnt == totalcnt)
-        {
-            string subans = s.substr(j + 1, i - j);
-            if (ans.size() == 0 || ans.size() > subans.size())
-            {
-                ans = subans;
+        int startind=0;
+        int endind=0;
+        
+        int ans=0;
+        
+        while(true){
+            bool flag1=false;
+            bool flag2=false;
+            
+            int size=s.size()-1;
+            
+            while(i<size && currcnt<totalcnt){
+                i++;
+                char ch=s[i];
+                map2[ch]++;
+                
+                if(map2[ch]<=map1[ch]){
+                    currcnt++;
+                }
+                flag1=true;
             }
-            j++;
-            char ch = s[j];
-            if (map2[ch] == 1)
-            {
-                map2.erase(ch);
+            
+            while(j<i && currcnt==totalcnt){
+                if(ans==0 || ans>(i-j)){
+                    ans=i-j;
+                    startind=j+1;
+                    endind=i;
+                }
+                j++;
+                char ch=s[j];
+                if(map2[ch]==1){
+                    map2.erase(ch);
+                }
+                else{
+                    map2[ch]--;
+                }
+                if(map1[ch]>map2[ch]){
+                    currcnt--;
+                }
+                flag2=true;
             }
-            else
-            {
-                map2[ch]--;
+            
+            if(flag1==false && flag2==false){
+                break;
             }
-            if (map1[ch] > map2[ch])
-            {
-                currcnt--;
-            }
-            flag2 = true;
         }
-
-        if (flag1 == false && flag2 == false)
-        {
-            break;
+        if(ans==0){
+            return "";
         }
+        string str=s.substr(startind,endind-startind+1);
+        return str;
     }
-    if (ans.size() == 0)
-    {
-        return "-1";
-    }
-
-    return ans;
-}
-;
+};
