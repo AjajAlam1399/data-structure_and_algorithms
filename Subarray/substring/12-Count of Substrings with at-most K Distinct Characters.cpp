c28fc159c4644e55c785a6b7146638bf15f1ef12
Ajaj Alam <2020umt1399@mnit.ac.in>
2023-01-25 12:54:32 +0530

    substring

---
+++ Subarray/substring/12-Count of Substrings with at-most K Distinct Characters.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <bits/stdc++.h>
+int getLengthofLongestSubstring(int k, string s)
+{
+    // Write your code here.
+    int i, j, n;
+    i = j = -1;
+    n = s.size();
+
+    int ans = 0;
+
+    unordered_map<char, int> map;
+
+    while (true)
+    {
+        bool f1, f2;
+        f1 = f2 = false;
+        while (i < n - 1)
+        {
+            f1 = true;
+            i++;
+            map[s[i]]++;
+            if (map.size() <= k)
+            {
+                ans+= i - j;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        while (j < i)
+        {
+            f2 = true;
+            j++;
+            if (map[s[j]] == 1)
+            {
+                map.erase(s[j]);
+            }
+            else
+            {
+                map[s[j]]--;
+            }
+            if (map.size() <= k)
+            {
+                ans+=i-j;
+                break;
+            }
+        }
+
+        if (!f1 && !f2)
+        {
+            break;
+        }
+    }
+    return ans;
+}
